5e14ce54ecd17b88e2c638ec5887b946bf6af0c0
Blessing Olusanya <debbyblessing1234@gmail.com>
2023-11-04 12:52:32 +0100

    Add linking attribute to the html

--- js/index.js
+++ js/index.js
@@ -1,9 +1,3 @@
-function sum(a, b) {
-  return a + b;
-}
-
-module.exports = sum;
-
 const startApp = () => {
   const body = document.querySelector('body');
   body.innerHTML = '<h2>JAVASCRIPT ENABLED</h2>';
