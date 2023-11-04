b089cdd2bac05a5d13f8b504de1ceba7bd56f2e4
Blessing Olusanya <debbyblessing1234@gmail.com>
2023-11-04 13:27:55 +0100

    fixing error

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
+function sum(a, b) {
+  return a + b;
+}
+
+module.exports = sum;
+
 const startApp = () => {
   const body = document.querySelector('body');
   body.innerHTML = '<h2>JAVASCRIPT ENABLED</h2>';
